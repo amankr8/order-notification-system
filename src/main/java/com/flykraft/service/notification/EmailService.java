2edeedef2c8c37377d5ed5360a93bdc5f53c125b
amankr8 <amankumar141099@gmail.com>
2025-12-20 00:02:54 +0530

    git update

--- src/main/java/com/flykraft/service/notification/EmailService.java
+++ src/main/java/com/flykraft/service/notification/EmailService.java
@@ -1,11 +1,13 @@
 package com.flykraft.service.notification;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class EmailService implements CommService {
 
     @Override
     public void sendNotification(String notification) {
-        System.out.println("[EMAIL]\t[" + LocalDateTime.now() + "][NOTIFICATION]" + notification);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println("[EMAIL]\t[NOTIFICATION][" + LocalDateTime.now().format(formatter) + "]" + notification);
     }
 }
--- src/main/java/com/flykraft/service/notification/SMSService.java
+++ src/main/java/com/flykraft/service/notification/SMSService.java
@@ -1,11 +1,13 @@
 package com.flykraft.service.notification;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SMSService implements CommService {
 
     @Override
     public void sendNotification(String notification) {
-        System.out.println("[SMS]\t[" + LocalDateTime.now() + "][NOTIFICATION]" + notification);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println("[SMS]\t[NOTIFICATION][" + LocalDateTime.now().format(formatter) + "]" + notification);
     }
 }
